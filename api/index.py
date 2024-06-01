293dfc8c2c53c5c80e6d7cbe79add3636d85557c
zhangkaihua88 <zhangkaihua@buaa.edu.cn>
2024-06-01 20:56:08 +0800

    g

--- api/index.py
+++ api/index.py
@@ -5,48 +5,45 @@ from joblib import Parallel, delayed
 from tqdm import tqdm
 import copy
 import requests
+import base64   
+from time import sleep
 
+
+ALL_SESS = {}
+BASE_AUTH = ''
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
-
+def wait_get(sess, url):
+    while True:
+        response = sess.get(url)
+        if response.headers.get("Retry-After", 0) == 0:
+            break
+        sleep(float(response.headers["Retry-After"]))
+    return response
 
 
 
 @app.route('/authentication', methods=['GET'])
 def login():
-    headers_keys = dict(request.headers)
-    
-    # 转换为列表（方便查看或其他操作）
-    # headers_list = list(headers_keys)
-    
-    return f"{headers_keys}"
-    # headers = request.headers.keys()
-    # username = "***REMOVED***"
-    # password = "***REMOVED***"
+    Authorization = request.headers.get('Authorization', BASE_AUTH)
+    Authorization = Authorization.split(' ')[-1]
 
-    # s = requests.Session()
-    # s.auth = (username, password)
-    # response = s.post('https://api.worldquantbrain.com/authentication')
-    # print(response)
-    # return type(headers)
-    # user_agent = request.headers.get('User-Agent')
-    
-    # Alternatively, you can use dictionary-like access
-    # host = request.headers['Host']
+    decoded_bytes = base64.b64decode(Authorization)
+    decoded_string = decoded_bytes.decode('utf-8')
+    username, password = decoded_string.split(':')
+    sess = requests.Session()
+    sess.auth = (username, password)
+    response = sess.post('https://api.worldquantbrain.com/authentication')
+    ALL_SESS[Authorization] = sess
+    return response.text
 
-    # return f"User-Agent: {user_agent}, Host: {host}"
+@app.route('/<path:path>', methods=['GET'])
+def redirect_to_other(path):
+    Authorization = request.headers.get('Authorization', BASE_AUTH)
+    Authorization = Authorization.split(' ')[-1]
+    response = wait_get(ALL_SESS[Authorization], f"https://api.worldquantbrain.com/{path}")
+    return response.text
 
-# @app.route('/<path:path>', methods=['GET', 'POST'])
-# def redirect_to_other(path):
-#     # 这里将所有除登录外的请求重定向到其他网站
-#     return redirect(f'https://otherwebsite.com/{path}')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
