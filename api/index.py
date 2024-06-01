42e73d8e40a619a9bb1eb2dbee11436369c64bc0
zhangkaihua88 <zhangkaihua@buaa.edu.cn>
2024-06-01 20:24:58 +0800

    g

--- api/index.py
+++ api/index.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import numpy as np
+# import optuna
+import json
+import joblib
+import pandas as pd
+from joblib import Parallel, delayed
+from tqdm import tqdm
+import copy
+import requests
+
 
 app = Flask(__name__)
 
@@ -9,3 +19,22 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+
+
+
+@app.route('/authentication', methods=['GET'])
+def login():
+    username = "***REMOVED***"
+    password = "***REMOVED***"
+
+    s = requests.Session()
+    s.auth = (username, password)
+    response = s.post('https://api.worldquantbrain.com/authentication')
+    print(response)
+    return '登录成功'
+
+# @app.route('/<path:path>', methods=['GET', 'POST'])
+# def redirect_to_other(path):
+#     # 这里将所有除登录外的请求重定向到其他网站
+#     return redirect(f'https://otherwebsite.com/{path}')
